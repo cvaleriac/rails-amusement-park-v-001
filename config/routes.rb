f4de9a2b1cfef820167e546cb3a06cce63a30899
Michael <michael.ries@gmail.com>
2017-05-11 16:49:51 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'users#welcome'
+  get '/signin' => 'users#signin', as: :signin
+  post '/login' => 'users#login', as: :login
+  get '/logout' => 'users#logout', as: :logout
+  resources :users, only: [:new, :show, :create, :edit, :update]
+  resources :attractions
+  post '/attractions/ride' => 'attractions#ride'
 
 end
